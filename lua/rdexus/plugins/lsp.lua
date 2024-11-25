b4418ec948a1bda09fa4c47b261356a9703644dd
Lorenzo <l.tenhagen@exonet.nl>
2024-11-25 09:43:22 +0100

    feat: remove lua and update typescript/javascript lsp

--- lua/rdexus/plugins/lsp.lua
+++ lua/rdexus/plugins/lsp.lua
@@ -8,8 +8,6 @@ return {
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
     },
 
@@ -26,11 +24,10 @@ return {
         require("mason").setup({})
         require("mason-lspconfig").setup({
             ensure_installed = {
-                "lua_ls",
                 "eslint",
                 "html",
                 "jsonls",
-                "tsserver",
+                "ts_ls",
                 "intelephense",
                 -- phpactor didn't work anymore for some reason so I'm using the free version of intelephense for the time being.
                 -- I do prefer the speed of intelephense over phpactor so I might get the premium version for more features.
@@ -42,29 +39,6 @@ return {
                         capabilities = capabilities
                     })
                 end,
-
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = {
-                                        "vim",
-                                        "Config",
-                                    }
-                                },
-                                workspace = {
-                                    library = {
-                                        vim.env.VIMRUNTIME,
-                                        "/home/dexus/repositories/fivem-lls-addon/addon/library",
-                                    },
-                                },
-                            }
-                        }
-                    }
-                end,
             }
         })
 
@@ -72,11 +46,6 @@ return {
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
@@ -85,7 +54,6 @@ return {
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
             }, {
                 { name = 'buffer' },
             })
