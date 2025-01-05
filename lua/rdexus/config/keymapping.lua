f2b664720b698f61aeb52d0d02346a9956f64026
rdexus <dexusonsteam@gmail.com>
2025-01-05 19:51:42 +0100

    feat: update keymapping.lua

--- lua/rdexus/config/keymapping.lua
+++ lua/rdexus/config/keymapping.lua
@@ -12,24 +12,6 @@ vim.keymap.set("n", "<leader>e", ":Explore<CR>", { noremap = true, silent = true
 -- close buffer
 vim.keymap.set("n", "<leader>bc", ":bd<CR>", { noremap = true, silent = true })
 
--- show open float diagnostic
-vim.keymap.set("n", "<leader>of", function()
-        -- THIS IS FOR BUILTIN LSP
-        vim.diagnostic.open_float(nil, {
-            scope = "cursor",
-            focusable = false,
-            close_events = {
-                "CursorMoved",
-                "CursorMovedI",
-                "BufHidden",
-                "InsertCharPre",
-                "WinLeave",
-            },
-        })
-    end,
-    { noremap = true, silent = true }
-)
-
 -- move focus to different windows
 vim.keymap.set('n', '<C-l>', '<C-W><C-L>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-k>', '<C-W><C-K>', { noremap = true, silent = true })
@@ -37,11 +19,11 @@ vim.keymap.set('n', '<C-j>', '<C-W><C-J>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-h>', '<C-W><C-H>', { noremap = true, silent = true })
 
 -- bufferline mapping
-vim.keymap.set("n", "<leader>bn", ":BufferLineCycleNext<CR>");
-vim.keymap.set("n", "<leader>bp", ":BufferLineCyclePrev<CR>");
-vim.keymap.set("n", "<leader>btp", ":BufferLineTogglePin<CR>");
-vim.keymap.set("n", "<leader>bmn", ":BufferLineMoveNext<CR>");
-vim.keymap.set("n", "<leader>bmp", ":BufferLineMovePrev<CR>");
+-- vim.keymap.set("n", "<leader>bn", ":BufferLineCycleNext<CR>");
+-- vim.keymap.set("n", "<leader>bp", ":BufferLineCyclePrev<CR>");
+-- vim.keymap.set("n", "<leader>btp", ":BufferLineTogglePin<CR>");
+-- vim.keymap.set("n", "<leader>bmn", ":BufferLineMoveNext<CR>");
+-- vim.keymap.set("n", "<leader>bmp", ":BufferLineMovePrev<CR>");
 
 -- undotree mapping
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
