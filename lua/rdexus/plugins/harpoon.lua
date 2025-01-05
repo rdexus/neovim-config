c9d4bda4e91c91535c109dace2d3b5ec65b47b13
rdexus <dexusonsteam@gmail.com>
2025-01-05 21:43:36 +0100

    feat: remove unused function

--- lua/rdexus/plugins/harpoon.lua
+++ lua/rdexus/plugins/harpoon.lua
@@ -10,29 +10,10 @@ return {
 
         harpoon:setup()
 
-        local conf = require("telescope.config").values
-        local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-                table.insert(file_paths, item.value)
-            end
-
-            require("telescope.pickers").new({}, {
-                prompt_title = "Harpoon",
-                finder = require("telescope.finders").new_table({
-                    results = file_paths,
-                }),
-                previewer = conf.file_previewer({}),
-                sorter = conf.generic_sorter({}),
-            }):find()
-        end
-
-
         local map = function(keys, func, desc)
             vim.keymap.set("n", keys, func, { desc = "Harpoon: " .. desc })
         end
 
-        -- map("<C-e>", function() toggle_telescope(harpoon:list()) end, "Open Harpoon Window")
         map("<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, "Open Harpoon Window")
         map("<leader>a", function() harpoon:list():add() end, "Add Buffer To List")
 
