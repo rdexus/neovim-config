9a963791ae1881874b81fa1a10290f54c7754e1b
Lorenzo <l.tenhagen@exonet.nl>
2025-01-11 16:40:35 +0100

    feat: add devicons to telescope

--- lua/rdexus/plugins/telescope.lua
+++ lua/rdexus/plugins/telescope.lua
@@ -1,31 +1,34 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-        require("telescope").setup({
-            defaults = require("telescope.themes").get_dropdown({
-                file_ignore_patterns = {
-                    "node_modules",
-                    "vendor",
-                    "dist",
-                    "build",
-                    "public",
-                },
-                prompt_prefix = "🔍 ",
-                sorting_strategy = "ascending",
-                layout_config = {
-                    width = 0.8,
-                    prompt_position = "top",
-                },
-            }),
-        })
-    end,
-    keys = {
-        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files",     mode = "n" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Telescope live grep",      mode = "n" },
-        { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Telescope buffers",        mode = "n" },
-        { "<leader>fo", "<cmd>Telescope oldfiles<cr>",   desc = "Telescope find old files", mode = "n" },
-        { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Vim help tags",            mode = "n" },
-    }
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("telescope").setup({
+			defaults = require("telescope.themes").get_dropdown({
+				file_ignore_patterns = {
+					"node_modules",
+					"vendor",
+					"dist",
+					"build",
+					"public",
+				},
+				prompt_prefix = "🔍 ",
+				sorting_strategy = "ascending",
+				layout_config = {
+					width = 0.8,
+					prompt_position = "top",
+				},
+			}),
+		})
+	end,
+	keys = {
+		{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files", mode = "n" },
+		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep", mode = "n" },
+		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers", mode = "n" },
+		{ "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Telescope find old files", mode = "n" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Vim help tags", mode = "n" },
+	},
 }
