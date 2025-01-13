bf560444076f2a16047c6f9a0abfe7842d72f2bf
Lorenzo <l.tenhagen@exonet.nl>
2025-01-13 13:09:08 +0000

    feat: update netrw settings

--- lua/rdexus/config/options.lua
+++ lua/rdexus/config/options.lua
@@ -39,12 +39,7 @@ vim.schedule(function()
 end)
 
 -- netrw
-vim.g.netrw_browse_split = 3
 vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
-vim.g.netrw_liststyle = 3
-vim.g.netrw_preview = 1
-vim.g.netrw_keepdir = 1
 
 -- open float diagnostic window
 vim.diagnostic.config({
